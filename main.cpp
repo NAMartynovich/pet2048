f2de635262950c12465272d392846fe269a45d27
NMartynovich <nmartynovich@gmail.com>
2026-04-20 22:30:39 +0300

    add main

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello, World!" << std::endl;
+  std::cout << "This is a test" << std::endl;
+  std::cout << "This is a test" << std::endl;
+  std::cout << "This is a test" << std::endl;
+  std::cout << "This is a test" << std::endl;
+}
